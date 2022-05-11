70ebe19d59f875fbb14e743de7c43497e6a1b610
Milton <Milton@192.168.1.192>
2022-05-11 09:25:26 -0300

    Atualização: Metodo para limpeza, organização do menu e correção de bug.

--- UrnaV2/src/Urna.java
+++ UrnaV2/src/Urna.java
@@ -1,15 +1,14 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Urna {
 
 	public static void main(String[] args) {
 		ArrayList<CandidatoTeste> Candidatos = new ArrayList<CandidatoTeste>();
 		Scanner leia = new Scanner(System.in);
-		int escolha, escolhaCandidato, numero, consulta, contadorVoto=0;
+		int escolha, escolhaCandidato, numero, consulta, contadorVoto = 0;
 		String nome, partido;
-		
+
 		do {
 			System.out.print("|-----------------------------|\n");
 			System.out.print("| Escolha uma das opções:     |\n");
@@ -31,9 +30,13 @@ public class Urna {
 				System.out.println("Digite o partido: ");
 				partido = leia.nextLine();
 				Candidatos.add(new CandidatoTeste(numero, nome, partido));
+				Urna.clearConsole("Candidato cadastrado.");
+
 				break;
 			case 2:
+				Urna.clearConsole("");
 				do {
+
 					System.out.print("|-----------------------------------|\n");
 					System.out.print("| Escolha uma das opções:           |\n");
 					System.out.print("| Opção 1 - Todos os candidatos     |\n");
@@ -43,63 +46,88 @@ public class Urna {
 					escolhaCandidato = leia.nextInt();
 
 					if (escolhaCandidato == 1) {
-						System.out.println("Seus candidatos são: ");
+						Urna.clearConsole("");
+						System.out.print("|-----------------------------------|\n");
+						System.out.print("| Seus candidato são:               |\n");
 						for (int i = 0; i < Candidatos.size(); i++) {
-							System.out.println("Nome: " + Candidatos.get(i).getNome() + ", Partido: "
+							System.out.println("| Nome: " + Candidatos.get(i).getNome() + ", Partido: "
 									+ Candidatos.get(i).getPartido() + ", Votos: " + Candidatos.get(i).getVotos());
+
 						}
-						System.out.println("Total de candidatos: " + Candidatos.size());
+						System.out.print("| Total de candidatos: " + Candidatos.size() + "\n");
+						System.out.print("|-----------------------------------|\n");
 					} else if (escolhaCandidato == 2) {
-						System.out.println("Digite o número do candidato");
+						Urna.clearConsole("");
+						System.out.print("|-----------------------------------|\n");
+						System.out.print("| Digite o número do candidato:     |\n");
 						consulta = leia.nextInt();
+						System.out.print("|-----------------------------------|\n");
 						for (int i = 0; i < Candidatos.size(); i++) {
 							if (Candidatos.get(i).getNumero() == consulta) {
-								System.out.println("Seu candidato é: ");
-								System.out.println("Nome: " + Candidatos.get(i).getNome() + ", Partido: "
+								System.out.println("| Seu candidato é: ");
+								System.out.println("| Nome: " + Candidatos.get(i).getNome() + ", Partido: "
 										+ Candidatos.get(i).getPartido() + ", Votos: " + Candidatos.get(i).getVotos());
+								System.out.print("|-----------------------------------|\n");
 								break;
 							}
 
 						}
+
 					}
 				} while (escolhaCandidato != 3);
-				System.out.println("Voltando ao menu principal.");
+
+				Urna.clearConsole("Voltando ao menu principal.");
 				break;
 			case 3:
-				System.out.println("Digite um número do candidato a ser votado.");
+				System.out.print("|---------------------------------------------|\n");
+				System.out.print("| Digite um número do candidato a ser votado: |\n");
 				consulta = leia.nextInt();
-				int i = 0;
-				for (; i < Candidatos.size(); i++) {
+				System.out.print("|---------------------------------------------|\n");
+				int i;
+				for (i = 0; i < Candidatos.size(); i++) {
 					if (Candidatos.get(i).getNumero() == consulta) {
 						break;
 					}
 				}
 				if (i != Candidatos.size()) {
 					Candidatos.get(i).Votar();
-				contadorVoto++;
-					System.out.println("Obrigado. Seu voto será a diferença que você quer fazer no mundo.");
+					contadorVoto++;
+					System.out.print("|-------------------------------------------------------------------|\n");
+					System.out.print("| Obrigado. Seu voto será a diferença que você quer fazer no mundo. |\n");
+					System.out.print("|-------------------------------------------------------------------|\n");
 				} else {
-					System.out.println("Candidato invalido!");
+					System.out.print("|-------------------------------------|\n");
+					System.out.print("| Candidato invalido!                 |\n");
+					System.out.print("|-------------------------------------|\n");
 				}
 				break;
 			case 4:
-				System.out.println("Validando votos...");
-				System.out.println("Votos validos: ");
+				Urna.clearConsole("");
+				System.out.print("|------------------------------------------|\n");
+				System.out.print("| Validando votos...                       |\n");
+				System.out.print("| Votos validos:                           |\n");
+
 				for (int j = 0; j < Candidatos.size(); j++) {
-					System.out.println("Nome: " + Candidatos.get(j).getNome() + ", Partido: "
+					System.out.println("| Nome: " + Candidatos.get(j).getNome() + ", Partido: "
 							+ Candidatos.get(j).getPartido() + ", Votos: " + Candidatos.get(j).getVotos());
 				}
-				System.out.println("Total de candidatos: " + Candidatos.size());
-				System.out.println("Total de votos: "+contadorVoto);
-				System.out.println("Obrigado a todos que votaram.");
+				System.out.print("|------------------------------------------|\n");
+				System.out.print("| Total de candidatos: " + Candidatos.size()+"\n");
+				System.out.print("| Total de votos: " + contadorVoto+ "\n");
+				System.out.print("| Obrigado a todos que votaram.            |\n");
+				System.out.print("|------------------------------------------|\n");
 				escolha = 5;
 				break;
 			case 5:
-				System.out.println("Saindo...");
-				System.out.println("Obrigado. Pela paciência.");
+				System.out.print("|---------------------------|\n");
+				System.out.print("| Saindo...                 |\n");
+				System.out.print("| Obrigado. Pela paciência. |\n");
+				System.out.print("|---------------------------|\n");
 				break;
 			default:
-				System.out.println("Opção invalida! Tente novamente.");
+				System.out.print("|-----------------------------------|\n");
+				System.out.print("| Opção invalida! Tente novamente.  |\n");
+				System.out.print("|-----------------------------------|\n");
 
 			}
 
@@ -108,4 +136,16 @@ public class Urna {
 		leia.close();
 
 	}
+
+	public static void clearConsole(String mensagem) {
+		for (int i = 0; i < 1050; ++i) {
+			System.out.println();
+		}
+		if (mensagem.length() > 0) {
+			System.out.print("|---------------------------------------------------|\n");
+			System.out.print("| " + mensagem + "\n");
+			System.out.print("|---------------------------------------------------|\n\n");
+		}
+	}
+
 }
